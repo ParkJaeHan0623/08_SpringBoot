68d9109727ffdfb58c96a022e36aa8adc36e36c3
ParkJaeHan <mitshowme@naver.com>
2024-12-31 19:21:57 +0900

    메일 코드 삭제

--- 13_MyShop/src/main/java/kr/parkjaehan/myshop/MyshopApplication.java
+++ 13_MyShop/src/main/java/kr/parkjaehan/myshop/MyshopApplication.java
@@ -2,13 +2,19 @@ package kr.parkjaehan.myshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // Scheduler를 사용하기 위한 어노테이션
 @EnableScheduling
 @SpringBootApplication
-public class MyshopApplication {
+public class MyshopApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyshopApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyshopApplication.class, args);
 	}
